bb180144fd1c8453f52d25f7c3263d0e237be27e
E. Semenyachenko <ybsemeny@mts.ru>
2020-02-25 17:53:00 +0300

    "write to db works"

--- logger.go
+++ logger.go
@@ -1,1 +1,93 @@
 package main
+
+import (
+	"database/sql"
+	_ "github.com/mattn/go-sqlite3"
+	"github.com/streadway/amqp"
+	//"io"
+	"log"
+	//"net/http"
+	"strings"
+	"sync"
+)
+
+const rabbitMqConnection = "amqp://guest:guest@localhost:5672/"
+
+var lock sync.Mutex
+var rlock sync.RWMutex
+
+type LogMessage struct {
+	datetime string
+	sender   string
+	message  string
+}
+
+func NewLogMessageFromRabbit(msg []byte) *LogMessage {
+	var l LogMessage
+	data := strings.Split(string(msg), "::")
+	l.datetime = data[0]
+	l.sender = data[1]
+	l.message = data[2]
+	return &l
+}
+
+func main() {
+	var err error
+	conn, err := amqp.Dial(rabbitMqConnection)
+	if err != nil {
+		log.Fatal("Failed to connect RabbitMQ")
+	}
+	channel, err := conn.Channel()
+	if err != nil {
+		log.Fatal("Failed to create channel RabbitMQ")
+	}
+	queue, err := channel.QueueDeclare(
+		"log",
+		true,
+		false,
+		false,
+		false,
+		nil,
+	)
+	messages, err := channel.Consume(
+		queue.Name,
+		"",
+		false,
+		false,
+		false,
+		false,
+		nil,
+	)
+	if err != nil {
+		log.Fatal("Failed to register a consumer")
+	}
+
+	db, err := sql.Open("sqlite3", "logbase.sqlite")
+	if err != nil {
+		log.Fatal("Can't open DB file! Error: ", err)
+	}
+	defer db.Close()
+	forever := make(chan bool)
+
+	go func() {
+		lock.Lock()
+		defer lock.Unlock()
+		for msg := range messages {
+			_ = msg.Ack(false)
+			lm := NewLogMessageFromRabbit(msg.Body)
+			_, err := db.Exec("Insert into log (datetime, sender, message) "+
+				"values ($1, $2, $3)", lm.datetime, lm.sender, lm.message)
+			if err != nil {
+				log.Println("Error writing to DB: ", err)
+			}
+		}
+	}()
+	<-forever
+}
+
+func ReturnNLogMessages(num int, page int, db sql.DB) []LogMessage {
+	rlock.RLock()
+	defer rlock.RUnlock()
+	rows, err := db.Query()
+
+}
